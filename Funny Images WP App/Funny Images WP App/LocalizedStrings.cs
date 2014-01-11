608f98f126368b06b425a26535f81a2794f10cf5
Rishav Binayak Das <rishavdas7@gmail.com>
2014-01-11 09:45:22 +0530

    basic app skeleton

---
+++ Funny Images WP App/Funny Images WP App/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Funny_Images_WP_App.Resources;
+
+namespace Funny_Images_WP_App
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
